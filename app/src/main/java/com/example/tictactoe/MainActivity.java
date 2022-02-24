009cd4727c381ac7d1d6ed80db5ce5ee0e2cc373
faozibouybaouenegadrouz <fbg171@gmail.com>
2022-02-24 19:14:36 +0100

    First listeners done

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,12 +3,109 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView r1c1;
+    private ImageView r1c2;
+    private ImageView r1c3;
+    private ImageView r2c1;
+    private ImageView r2c2;
+    private ImageView r2c3;
+    private ImageView r3c1;
+    private ImageView r3c2;
+    private ImageView r3c3;
+
+    private TextView tvPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        r1c1 = findViewById(R.id.r1c1);
+        r1c2 = findViewById(R.id.r1c2);
+        r1c3 = findViewById(R.id.r1c3);
+        r2c1 = findViewById(R.id.r2c1);
+        r2c2 = findViewById(R.id.r2c2);
+        r2c3 = findViewById(R.id.r2c3);
+        r3c1 = findViewById(R.id.r3c1);
+        r3c2 = findViewById(R.id.r3c2);
+        r3c3 = findViewById(R.id.r3c3);
+
+        tvPlayer = findViewById(R.id.playerMessage);
+
+        tvPlayer.setText(R.string.player_1);
+
+        r1c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        r1c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        r1c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        r2c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        r2c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        r2c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        r3c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        r3c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        r3c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
