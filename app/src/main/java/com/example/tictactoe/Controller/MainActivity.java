b9c9c674a4fb531aa6616e3990c98abba7644aca
faozibouybaouenegadrouz <fbg171@gmail.com>
2022-02-25 16:04:36 +0100

    Started logic on playing. And Full cell notification

--- app/src/main/java/com/example/tictactoe/Controller/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/Controller/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tictactoe.Model.Casilla;
 import com.example.tictactoe.Model.Tablero;
@@ -28,10 +29,12 @@ public class MainActivity extends AppCompatActivity {
         ImageView r3c2 = findViewById(R.id.r3c2);
         ImageView r3c3 = findViewById(R.id.r3c3);
 
+        final int[] player = {1};
+
+
         Tablero tablero = new Tablero(r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3);
 
         TextView tvPlayer = findViewById(R.id.playerMessage);
-
         tvPlayer.setText(R.string.player_1);
 
         //TODO: como sabemos que player está jugando y le metemos el dibujo
@@ -42,8 +45,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Casilla casilla11 = tablero.getCasillaById(1);
                 if(casilla11.isEmpty()) {
-                    casilla11.getImageView().setImageResource(R.drawable.dot);
-                    casilla11.fill();
+
+                    if (player[0] == 1){
+                        casilla11.getImageView().setImageResource(R.drawable.dot);
+                        casilla11.fill();
+                    } else if (player[0] == 2){
+                        casilla11.getImageView().setImageResource(R.drawable.cross);
+                        casilla11.fill();
+                    }
+                    player[0] = setPlayer(player[0],tvPlayer);
+                } else {
+                    Toast.makeText(getApplicationContext(),R.string.cell_full,Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -54,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Casilla casilla12 = tablero.getCasillaById(2);
+                player[0] = setPlayer(player[0],tvPlayer);
                 if(casilla12.isEmpty()) {
                     casilla12.getImageView().setImageResource(R.drawable.cross);
                     casilla12.fill();
@@ -110,4 +123,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    private int setPlayer(int player,TextView tvPlayer ){
+        if (player == 1){
+            tvPlayer.setText(R.string.player_2);
+            player++;
+        } else if (player == 2){
+            tvPlayer.setText(R.string.player_1);
+            player--;
+        }
+        return player;
+    }
+
 }
